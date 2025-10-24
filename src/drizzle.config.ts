03060ada219150161560fa55a3cdc408fbbb9281
Kanha Dewangan <166921193+kanhadewangan@users.noreply.github.com>
2025-10-24 17:59:08 +0530

    Update drizzle.config.ts

--- src/drizzle.config.ts
+++ src/drizzle.config.ts
@@ -6,6 +6,6 @@
     schema: './db/schema.ts',
     dialect: 'postgresql',
     dbCredentials: {
-      url: "postgres://825ae35d56d29c8f00f018e3f0a75df4f0c05195c6155ba6d854133ac5dccde2:sk_SRv2ZIn0n927QiPmQK4l6@db.prisma.io:5432/postgres?sslmode=require",
+      url: process.env.url
     },
   });
